5a9d7f5e106609fbf2644978212cbad2da5c7c8e
boikedamhuis <boike.damhuis@me.com>
2014-01-18 14:14:42 +0100

    Versie1

---
+++ NewDirectory/MID2/MID/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  MID
+//
+//  Created by Boike Damhuis on 17-01-14.
+//  Copyright (c) 2014 Cawwi Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+# import "TFTTapForTap.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewDirectory/MID2/MID/ViewController.h
@@ -0,0 +1,34 @@
+//
+//  ViewController.h
+//  MID
+//
+//  Created by Boike Damhuis on 17-01-14.
+//  Copyright (c) 2014 Cawwi Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AdSupport/AdSupport.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+#import "TFTAppWall.h"
+
+@interface ViewController : UIViewController {
+    UIViewController *firstViewController;
+
+    //Input
+    
+    __weak IBOutlet UITextField *StandaardGlazen;
+    __weak IBOutlet UITextField *AantalUren;
+    //calc
+    
+    __weak IBOutlet UIButton *CalculateBtn;
+    //output
+    __weak IBOutlet UILabel *PromileLabel;
+    
+    float manvrouw;
+    
+    
+}
+- (IBAction)CalculatePressed:(id)sender;
+
+
+@end
