ed6700d3b04ec6fc80247135a048f1c0180e199a
boikedamhuis <boike.damhuis@me.com>
2014-01-25 15:05:47 +0100

    Bugs en Notificaties Update

--- NewDirectory/MID2/MID/SettingsViewController.m
+++ NewDirectory/MID2/MID/SettingsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SettingsViewController.h"
+#import <UPPlatformSDK/UPPlatformSDK.h>
 
 
 
--- NewDirectory/MID2/MID/ViewController.h
+++ NewDirectory/MID2/MID/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <AudioToolbox/AudioToolbox.h>
+#import <UPPlatformSDK/UPPlatformSDK.h>
 
 
 @interface ViewController : UIViewController <UIAlertViewDelegate> {
